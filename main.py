0378d6ac19111a1cb97a2647a30aba8700f7717d
nguyenquan12121 <quan177013@gmail.com>
2025-09-24 15:17:35 +0200

    added styling + routing

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from typing import Union
 from dotenv import load_dotenv
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 import requests
 import os
-
+import re
 app = FastAPI()
 
 GITHUB_REPO = "https://github.com/strawberrymusicplayer/strawberry/issues"
@@ -22,15 +22,7 @@ YOUTRACK_REPO = "https://quan.youtrack.cloud/api/admin/projects?fields=id,name,s
 YOUTRACK_REPO_GET_FIELDS= "https://quan.youtrack.cloud/api/admin/projects?fields=assignee"
 def github_req():
     global GITHUB_ISSUE, GITHUB_TOKEN
-    request = {    "url": GITHUB_ISSUE, 
-               "headers": {
-        "header": "Accept: application/vnd.github+json" ,
-        "header": f'Authorization:{GITHUB_TOKEN}' 
-               }
-    }
-    response = requests.get(url=request["url"], headers=request["headers"])
-    data = response.json()
-    return data
+
 #curl -X GET \
 #'https://quan.youtrack.cloud/api/users/me' \                                                                            -H 'Authorization: Bearer perm-YWRtaW4=.NDQtMQ==.bEVtcOJ9b8462q7wtiRukDPu7bIasd' \
 #-H 'Accept: application/json' \
@@ -81,15 +73,37 @@ def youtrack_req():
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-async def index(request: Request):
-    # pass anything you want to the template as context
-    context = {"request": request, "title": "My FastAPI HTML Page", "items": ["apples", "bananas", "oranges"]}
-    return templates.TemplateResponse("index.html", context)
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "github": None, "submitted": False, "error": False})
 
 
-@app.get("/get_issues")
-def read_root():
-    return github_req()
+@app.post("/", response_class=HTMLResponse)
+async def handle_form(request: Request, github: str = Form(...)):
+    github = github.strip()
+    GITHUB_ISSUE_REGEX = re.compile(
+    r"^https:\/\/github\.com\/[A-Za-z0-9_.-]+\/[A-Za-z0-9_.-]+\/issues\/?$"    
+    )    
+    if not github:
+        error_msg = "URL cannot be empty."
+        return templates.TemplateResponse(
+            "index.html", {"request": request, "error": error_msg, "github": None, "submitted": True}
+        )
+
+    elif not GITHUB_ISSUE_REGEX.match(github):
+        error_msg = "Invalid GitHub issues URL. Must be like: https://github.com/user/repo/issues"
+        return templates.TemplateResponse(
+            "index.html", {"request": request, "error": error_msg, "github": None, "submitted": True}
+        )       
+    else:
+        request1 = {    "url": github, 
+                   "headers": {
+            "header": "Accept: application/vnd.github+json" ,
+            "header": f'Authorization:{GITHUB_TOKEN}' 
+                   }
+        }
+        response = requests.get(url=request["url"], headers=request["headers"])
+        data = response.json()
+        return templates.TemplateResponse("index.html", {"request": request, "github": github, "submitted": True})
 
 
